9d223f85bb62f77cb30e4d0c1d71bae4954c5a4d
Alberto Monteiro <alberto.monteiro@live.com>
2015-06-24 14:12:42 -0300

    projeto inicial

---
+++ src/dashing.net/App_Start/WebApiConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace dashing.net
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute(
+                name: "EventsApi",
+                routeTemplate: "api/{controller}"
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "Widget",
+                routeTemplate: "widgets/{id}",
+                defaults: new { controller = "Widgets" });
+        }
+    }
+}
